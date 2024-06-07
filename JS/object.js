cfa89987bd127415ad77eb24b317bc1eae464e35
SaeedUrRehman101 <saeedtutorial786@gmail.com>
2024-06-07 09:49:38 +0500

    first work

---
+++ JS/object.js
@@ -0,0 +1,15 @@
+class batch202309b{
+    constructor(name,course,semester){
+        this.name=name;
+        this.course=course;
+        this.semester=semester;
+    }
+    learning(name,course,semester){
+        let x=name+" is the student of " +course + "and current semester is "+ semester;
+        document.getElementsByClassName("object_heading").innerHTML=x;
+        console.log(x);
+    }
+}
+let std=new batch202309b;
+std.learning("Fahad","ACCP","2023");
+
